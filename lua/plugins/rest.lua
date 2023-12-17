57684d2332ece24bdca2bcf4b86aeb81be2a95a1
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-16 19:33:23 -0500

    Update rest.lua

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,6 +1,18 @@
+local is_http = function()
+  if vim.bo.filetype ~= 'http' then
+    require("tm10ymhp.utils").notify(
+      'RestNvim is only supported for HTTP requests',
+      vim.log.levels.ERROR
+    )
+
+    return false
+  end
+
+  return true
+end
+
 return {
   "rest-nvim/rest.nvim",
-  event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim" },
   init = function()
     vim.filetype.add({
@@ -9,41 +21,36 @@ return {
       }
     })
   end,
-  config = function()
-    require("rest-nvim").setup({
-      result_split_horizontal = true,
-      result = {
-        show_curl_command = true,
-      }
-    })
-
-    local is_http = function()
-      if vim.bo.filetype ~= 'http' then
-        require("tm10ymhp.utils").notify(
-          'RestNvim is only supported for HTTP requests',
-          vim.log.levels.ERROR
-        )
-
-        return false
-      end
-
-      return true
-    end
-
-    vim.keymap.set('n', '<leader>rr', function()
-      if is_http() then
-        require("rest-nvim").run()
-      end
-    end, { desc = "RestNvim Run" })
-    vim.keymap.set('n', '<leader>rl', function()
-      if is_http() then
-        require("rest-nvim").last()
-      end
-    end, { desc = "RestNvim Last" })
-    vim.keymap.set('n', '<leader>rp', function()
-      if is_http() then
-        require("rest-nvim").run(true)
-      end
-    end, { desc = "RestNvim Preview" })
+  opts = {
+    result_split_horizontal = true,
+    result = {
+      show_curl_command = true,
+    }
+  },
+  keys = {
+    {
+      '<leader>rr',
+      function()
+        if is_http() then require("rest-nvim").run() end
+      end,
+      desc = "RestNvim Run"
+    },
+    {
+      '<leader>rl',
+      function()
+        if is_http() then require("rest-nvim").last() end
+      end,
+      desc = "RestNvim Last"
+    },
+    {
+      '<leader>rp',
+      function()
+        if is_http() then require("rest-nvim").run(true) end
+      end,
+      desc = "RestNvim Preview"
+    },
+  },
+  config = function(_, opts)
+    require("rest-nvim").setup(opts)
   end
 }
