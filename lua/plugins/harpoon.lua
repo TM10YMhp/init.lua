8550b58176b63b70c1a6a6b72b75df9ec4b93fe0
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-16 23:39:17 -0500

    Update harpoon.lua

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,50 +1,36 @@
 return {
-  {
-    "ThePrimeagen/harpoon",
-    event = "VeryLazy",
-    config = function()
-      require("harpoon").setup()
-
-      local mark = require("harpoon.mark")
-      local ui = require("harpoon.ui")
-
-      vim.keymap.set(
-        "n",
-        "<leader>a",
-        mark.add_file,
-        { desc = "Harpoon: Add File" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>em",
-        ui.toggle_quick_menu,
-        { desc = "Harpoon: Menu" }
-      )
-
-      vim.keymap.set(
-        "n",
-        "<leader>1",
-        function() ui.nav_file(1) end,
-        { desc = "Harpoon: Nav File 1" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>2",
-        function() ui.nav_file(2) end,
-        { desc = "Harpoon: Nav File 2" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>3",
-        function() ui.nav_file(3) end,
-        { desc = "Harpoon: Nav File 3" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>4",
-        function() ui.nav_file(4) end,
-        { desc = "Harpoon: Nav File 4" }
-      )
-    end
+  "ThePrimeagen/harpoon",
+  keys = {
+    {
+      "<leader>ma",
+      function() require('harpoon.mark').add_file() end,
+      desc = "Harpoon: Add File"
+    },
+    {
+      "<leader>mm",
+      function() require("harpoon.ui").toggle_quick_menu() end,
+      desc = "Harpoon: Menu"
+    },
+    {
+      "<leader>1",
+      function() require("harpoon.ui").nav_file(1) end,
+      desc = "Harpoon: Nav File 1"
+    },
+    {
+      "<leader>2",
+      function() require("harpoon.ui").nav_file(2) end,
+      desc = "Harpoon: Nav File 2"
+    },
+    {
+      "<leader>3",
+      function() require("harpoon.ui").nav_file(3) end,
+      desc = "Harpoon: Nav File 3"
+    },
+    {
+      "<leader>4",
+      function() require("harpoon.ui").nav_file(4) end,
+      desc = "Harpoon: Nav File 4"
+    }
   },
+  opts = {},
 }
