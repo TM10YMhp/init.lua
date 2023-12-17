1f0859d53a8555f380d03c1e22b40afd1173b0e7
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-17 01:36:54 -0500

    Update osc52.lua

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,22 +1,24 @@
+-- Here is a non-exhaustive list of the status of popular terminal
+-- emulators regarding OSC52  (https://github.com/ojroques/vim-oscyank)
+--
+-- If you are using tmux, run these steps first: enabling OSC52 in tmux.
+-- Then make sure set-clipboard is set to on: set -s set-clipboard on.
+
 return {
   "ojroques/nvim-osc52",
-  event = "VeryLazy",
-  config = function()
-
-    --[[
-    Here is a non-exhaustive list of the status of popular terminal
-    emulators regarding OSC52  (https://github.com/ojroques/vim-oscyank)
-
-    If you are using tmux, run these steps first: enabling OSC52 in tmux.
-    Then make sure set-clipboard is set to on: set -s set-clipboard on.
-    ]]
-
-    local osc52 = require('osc52')
-    osc52.setup {
-      max_length = 0, --Maximum length of selection (0 for no limit)
-      silent = false, --Disable message on successful copy
-      trim = false,   --Trim text before copy
+  keys = {
+    {
+      '<leader>y',
+      function() require('osc52').copy_visual() end,
+      mode = 'x'
     }
-    vim.keymap.set('x', '<leader>y', osc52.copy_visual)
+  },
+  opts = {
+    max_length = 0, --Maximum length of selection (0 for no limit)
+    silent = false, --Disable message on successful copy
+    trim = false,   --Trim text before copy
+  },
+  config = function(_, opts)
+    require('osc52').setup(opts)
   end
 }
