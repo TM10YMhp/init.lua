03c49f7c74f99ff4f26bd87ae4f4f7c86ad85444
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-16 19:07:03 -0500

    Update mini.lua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,73 +1,38 @@
 return {
   {
     "echasnovski/mini.completion",
-    -- enabled = false,
-    event = "VeryLazy",
-    config = function()
-      -- local keys = {
-      --   ['cr']      = vim.api.nvim_replace_termcodes('<CR>', true, true, true),
-      --   ['c-y']     = vim.api.nvim_replace_termcodes('<C-y>', true, true, true),
-      --   ['c-n_c-y'] = vim.api.nvim_replace_termcodes('<C-n><C-y>', true, true, true),
-      -- }
-
-      -- local cr_action = function()
-      --   if vim.fn.pumvisible() ~= 0 then
-      --     local item_selected = vim.fn.complete_info()['selected'] ~= -1
-      --     return item_selected and keys['c-y'] or keys['c-n_c-y']
-      --   else
-      --     return keys['cr']
-      --   end
-      -- end
-
-      -- vim.keymap.set('i', '<CR>', cr_action, {
-      --   desc = "Completion: Confirm", expr = true
-      -- })
-      -- vim.keymap.set('i', '<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]], {
-      --   desc = "Completion: Next Item", expr = true
-      -- })
-      -- vim.keymap.set('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<Tab>"]], {
-      --   desc = "Completion: Prev Item", expr = true
-      -- })
-
-      local mini_completion = require("mini.completion")
-      mini_completion.setup({
-        delay = { completion = 1000 * 60 * 5 },
-        window = {
-          -- info = { border = "single" },
-          signature = { border = "single" },
-        },
-        lsp_completion = {
-          auto_setup = false,
-          -- process_items = function(items, base)
-          --   local res = mini_completion.default_process_items(items, base)
-          --   for _, item in pairs(res) do item.detail = '' end
-          --   return res
-          -- end
-        },
-        mappings = {
-          force_twostep = "",
-          force_fallback = "",
-        },
-        set_vim_settings = false
-      })
-
-      -- vim.opt.completefunc = 'v:lua.MiniCompletion.completefunc_lsp'
-    end
+    event = "InsertEnter",
+    opts = {
+      delay = { completion = 1000 * 60 * 5 },
+      window = {
+        info = { border = "single" },
+        signature = { border = "single" },
+      },
+      lsp_completion = {
+        auto_setup = false,
+      },
+      mappings = {
+        force_twostep = "",
+        force_fallback = "",
+      },
+      set_vim_settings = false
+    },
   },
   {
     "echasnovski/mini.trailspace",
     event = "VeryLazy",
-    config = function()
-      local mini_trailspace = require('mini.trailspace')
-      mini_trailspace.setup()
-
-      vim.keymap.set({'n', 'v'}, '<leader>ct',
+    keys = {
+      {
+        '<leader>ct',
         function()
-          mini_trailspace.trim()
-          mini_trailspace.trim_last_lines()
+          MiniTrailspace.trim()
+          MiniTrailspace.trim_last_lines()
         end,
-        { desc = "Trim All" }
-      )
+        desc = "Trim All"
+      }
+    },
+    config = function()
+      require('mini.trailspace').setup()
 
       vim.api.nvim_create_autocmd('FileType', {
         pattern = 'lazy',
@@ -81,12 +46,13 @@ return {
   {
     "echasnovski/mini.tabline",
     event = "VeryLazy",
-    config = function()
-      require('mini.tabline').setup({
-        show_icons = false,
-        set_vim_settings = false,
-        tabpage_section = 'right'
-      })
+    opts = {
+      show_icons = false,
+      set_vim_settings = false,
+      tabpage_section = 'right'
+    },
+    config = function(_, opts)
+      require('mini.tabline').setup(opts)
 
       vim.opt.showtabline = 2
     end
@@ -133,23 +99,37 @@ return {
   },
   {
     "echasnovski/mini.jump2d",
-    event = "VeryLazy",
-    config = function()
-      require('mini.jump2d').setup({
-        mappings = { start_jumping = "" }
-      })
-
-      vim.keymap.set(
-        { "n", "o", "x" },
+    keys = {
+      {
         "<cr>",
         "<cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.single_character)<CR>",
-        { desc = "Start 2d jumping" }
-      )
-    end
+        mode = { "n", "x" },
+        desc = "Start 2d jumping",
+      }
+    },
+    opts = {
+      mappings = { start_jumping = "" }
+    },
+  },
+  {
+    "echasnovski/mini.align",
+    event = "VeryLazy",
+    opts = {}
+  },
+  {
+    "echasnovski/mini.move",
+    event = "VeryLazy",
+    opts = {}
   },
   {
     "echasnovski/mini.files",
-    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>E",
+        "<cmd>lua MiniFiles.open(vim.fn.expand('%:p:h'))<CR>",
+        desc = "Create files"
+      }
+    },
     config = function()
       local my_prefix = function(fs_entry)
         if fs_entry.fs_type == 'directory' then
@@ -241,23 +221,6 @@ return {
           map('-', custom_go_out_plus, 'Custom go out plus')
         end,
       })
-
-      vim.keymap.set(
-        'n',
-        '<leader>E',
-        "<cmd>lua MiniFiles.open(vim.fn.expand('%:p:h'))<CR>",
-        { desc = "Create files" }
-      )
     end
   },
-  {
-    "echasnovski/mini.align",
-    event = "VeryLazy",
-    opts = {}
-  },
-  {
-    "echasnovski/mini.move",
-    event = "VeryLazy",
-    opts = {}
-  }
 }
