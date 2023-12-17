a985ef047c3ac43ab116bf6430bd0ccb9e2d4eb8
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-17 02:18:31 -0500

    Update lazy.lua

--- lua/tm10ymhp/lazy.lua
+++ lua/tm10ymhp/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
   defaults = { lazy = true },
   dev = { path = "~/projects" },
+  install = { colorscheme = { "serene" } },
   ui = {
     size = { width = 90, height = 40 },
     wrap = true,
