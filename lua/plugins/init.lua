8dd1068e3f58ad3e72543f1d78db66d4a55c606b
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-20 09:58:32 -0500

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -83,24 +83,4 @@ return {
       vim.cmd("silent Sleuth")
     end
   },
-  {
-    "stevearc/dressing.nvim",
-    event = "VeryLazy",
-    opts = {
-      input = {
-        border = "single",
-        relative = "editor",
-        max_width = 120,
-        min_width = 60,
-      },
-      select = {
-        backend = { "builtin" },
-        builtin = {
-          border = "single",
-          max_width = 120,
-          min_width = 60,
-        }
-      }
-    },
-  },
 }
