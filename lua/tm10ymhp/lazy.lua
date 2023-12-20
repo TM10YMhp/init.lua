d30cef10de9d571b428ce0e59a0afd939c7a2c01
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-19 20:19:13 -0500

    Update lazy.lua

--- lua/tm10ymhp/lazy.lua
+++ lua/tm10ymhp/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
     { import = "plugins.mini" },
+    { import = "plugins.git" },
   },
   dev = { path = "~/projects" },
   ui = {
