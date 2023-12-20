932d7479b24a4561a0aa532d9d8cd4d46da93b44
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-20 11:33:03 -0500

    Update telescope_extensions.lua

--- lua/plugins/telescope_extensions.lua
+++ lua/plugins/telescope_extensions.lua
@@ -1,78 +1,90 @@
 return {
-  "telescope.nvim",
-  opts = function(_, opts)
-    return vim.tbl_deep_extend("force", opts, {
-      extensions = {
-        fzf = {
-          fuzzy = false,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = "smart_case"
-        },
-        undo = {
-          use_delta = false,
-        },
-      }
-    })
-  end,
-  dependencies = {
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
-      config = function()
-        require("telescope").load_extension("fzf")
-      end
-    },
-    {
-      "nvim-telescope/telescope-symbols.nvim",
-      keys = {
-        { "<leader>ss", "<cmd>Telescope symbols<cr>", desc = "Symbols" },
-        {
-          "<leader>se",
-          "<cmd>lua require'telescope.builtin'.symbols{sources={'emoji'}}<cr>",
-          desc = "Emoji",
-        },
-        {
-          "<leader>sk",
-          "<cmd>lua require'telescope.builtin'.symbols{sources={'kaomoji'}}<cr>",
-          desc = "Kaomoji",
-        },
-        {
-          "<leader>sg",
-          "<cmd>lua require'telescope.builtin'.symbols{sources={'gitmoji'}}<cr>",
-          desc = "Gitmoji",
-        },
-      }
-    },
-    {
-      "debugloop/telescope-undo.nvim",
-      keys = {
-        { "<leader>eu", "<cmd>Telescope undo<cr>", desc = "Undo" },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, {
+        extensions = {
+          fzf = {
+            fuzzy = false,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case"
+          }
+        }
+      })
+    end,
+    dependencies = {
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        config = function()
+          require("telescope").load_extension("fzf")
+        end
       },
-      config = function()
-        require("telescope").load_extension("undo")
-      end
     },
-    {
-      "nvim-telescope/telescope-live-grep-args.nvim",
-      keys = {
-        { "<leader>elG", "<cmd>Telescope live_grep_args<cr>", desc = "Live Grep Args" },
+  },
+  {
+    "nvim-telescope/telescope-symbols.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    keys = {
+      { "<leader>ss", "<cmd>Telescope symbols<cr>", desc = "Symbols" },
+      {
+        "<leader>se",
+        "<cmd>lua require'telescope.builtin'.symbols{sources={'emoji'}}<cr>",
+        desc = "Emoji",
       },
-      config = function()
-        require("telescope").load_extension("live_grep_args")
-      end
+      {
+        "<leader>sk",
+        "<cmd>lua require'telescope.builtin'.symbols{sources={'kaomoji'}}<cr>",
+        desc = "Kaomoji",
+      },
+      {
+        "<leader>sg",
+        "<cmd>lua require'telescope.builtin'.symbols{sources={'gitmoji'}}<cr>",
+        desc = "Gitmoji",
+      },
     },
-    {
-      "alan-w-255/telescope-mru.nvim",
-      dependencies = {
-        { "yegappan/mru", event = "VeryLazy" },
-      },
-      keys = {
-        { "<leader>eo", "<cmd>Telescope mru<cr>", desc = "MRU" },
-      },
-      config = function()
-        require("telescope").load_extension("mru")
-      end
+  },
+  {
+    "debugloop/telescope-undo.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      opts = function(_, opts)
+        return vim.tbl_deep_extend("force", opts, {
+          extensions = {
+            undo = { use_delta = false }
+          }
+        })
+      end,
     },
+    keys = {
+      { "<leader>eu", "<cmd>Telescope undo<cr>", desc = "Undo" },
+    },
+    config = function()
+      require("telescope").load_extension("undo")
+    end
+  },
+  {
+    "alan-w-255/telescope-mru.nvim",
+    dependencies = {
+      { "yegappan/mru", event = "VeryLazy" },
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>eo", "<cmd>Telescope mru<cr>", desc = "MRU" },
+    },
+    config = function()
+      require("telescope").load_extension("mru")
+    end
+  },
+  {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    keys = {
+      { "<leader>elG", "<cmd>Telescope live_grep_args<cr>", desc = "Live Grep Args" },
+    },
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end
   },
 }
