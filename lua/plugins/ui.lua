8c29922702f8472a1c542a3bfaf482c2449e3f6c
tm10ymhp <tm10ymhp@protonmail.com>
2023-12-21 18:31:37 -0500

    dressign

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,19 +1,21 @@
 return {
-  "stevearc/dressing.nvim",
-  event = "VeryLazy",
-  opts = {
-    input = {
-      border = "single",
-      relative = "editor",
-      max_width = 120,
-      min_width = 60,
-    },
-    select = {
-      backend = { "builtin" },
-      builtin = {
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    opts = {
+      input = {
         border = "single",
+        relative = "editor",
         max_width = 120,
         min_width = 60,
+      },
+      select = {
+        backend = { "builtin" },
+        builtin = {
+          border = "single",
+          max_width = 120,
+          min_width = 60,
+        }
       }
     }
   },
