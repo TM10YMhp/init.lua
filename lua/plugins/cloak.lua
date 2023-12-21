725b7d0f2dbbedb1633ac7cfdd2fc171552c97fc
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-21 13:13:21 -0500

    Update cloak.lua

--- lua/plugins/cloak.lua
+++ lua/plugins/cloak.lua
@@ -1,21 +1,6 @@
 return {
   "laytan/cloak.nvim",
   ft = "dotenv",
-  init = function()
-    vim.filetype.add({
-      extension = {
-        env = "dotenv",
-      },
-      filename = {
-        [".env"] = "dotenv",
-        ["env"] = "dotenv",
-      },
-      pattern = {
-        -- INFO: Match filenames like - ".env.example", ".env.local" and so on
-        ["%.env%.[%w_.-]+"] = "dotenv",
-      },
-    })
-  end,
   keys = {
     { '<leader>uC', '<cmd>CloakToggle<cr>', desc = "Toggle Cloak" }
   },
