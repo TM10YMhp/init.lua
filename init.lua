a3040de2251c96fefa958fb5e16d3b9b226892a9
tm10ymhp <tm10ymhp@protonmail.com>
2023-12-21 17:47:32 -0500

    globals

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
-require('tm10ymhp.globals')
-require('tm10ymhp.autocmds')
 require('tm10ymhp.init')
+require('tm10ymhp.autocmds')
 require('tm10ymhp.base')
 require('tm10ymhp.maps')
 require('tm10ymhp.diagnostic')
--- lua/tm10ymhp/init.lua
+++ lua/tm10ymhp/init.lua
@@ -1,5 +1,27 @@
 local config_path = vim.fn.stdpath("config") .. "/lua/styles/"
 
+-- tm10ymhp
+vim.g.mapleader = ","
+
+vim.g.netrw_fastbrowse = 0
+vim.g.netrw_liststyle = 0
+vim.g.netrw_mousemaps = 0
+vim.g.netrw_winsize = 30
+
+vim.g.matchparen_timeout = 20
+vim.g.matchparen_insert_timeout = 20
+vim.g.loaded_matchparen = 1
+
+vim.g.loaded_matchit = 1
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_zip       = 1
+
+vim.g.markdown_recommended_style = 0
+
 -- cloak.nvim
 vim.filetype.add({
   extension = {
