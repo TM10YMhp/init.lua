142dbfdf28c03e8dcfccf760ef57eea8b56ed702
tm10ymhp <tm10ymhp@protonmail.com>
2023-12-22 16:24:07 -0500

    23.1222.1623

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require('tm10ymhp.init')
 require('tm10ymhp.autocmds')
 require('tm10ymhp.base')
-require('tm10ymhp.maps')
+require('tm10ymhp.keymaps')
 require('tm10ymhp.diagnostic')
 require('tm10ymhp.lazy')
