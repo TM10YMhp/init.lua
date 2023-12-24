7d1705455e9af1c2bcad5f4d5c28421eec4cdb84
tm10ymhp <tm10ymhp@protonmail.com>
2023-12-24 14:29:49 -0500

    fugitive log

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,12 +1,31 @@
 return {
-  "tpope/vim-fugitive",
-  cmd = { "Git", "G" },
-  keys = {
-    {
-      "<leader>gb",
-      ":G blame<cr>",
-      mode = { "n", "x" },
-      desc = "Git Blame"
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "G" },
+    keys = {
+      {
+        "<leader>gb",
+        ":G blame<cr>",
+        mode = { "n", "x" },
+        desc = "Git Blame"
+      },
+    },
+  },
+  {
+    "rbong/vim-flog",
+    dependencies = { "tpope/vim-fugitive" },
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+    keys = {
+      {
+        "<leader>gl",
+        "<cmd>Flogsplit<cr>",
+        desc = "Git Log",
+      },
+      {
+        "<leader>gL",
+        "<cmd>Flogsplit -raw-args=--follow -path=%<cr>",
+        desc = "Git Log Curent File",
+      },
     },
   },
 }
--- lua/tm10ymhp/init.lua
+++ lua/tm10ymhp/init.lua
@@ -77,3 +77,8 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- flog
+vim.g.flog_default_opts = {
+  max_count = 1000,
+}
