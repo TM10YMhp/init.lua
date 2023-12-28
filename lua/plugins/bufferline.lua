5554c8076328c0da75229b68d666411c0c254da2
tm10ymhp <tm10ymhp@protonmail.com>
2023-12-27 20:06:25 -0500

    bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,38 +1,39 @@
 return {
   {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "echasnovski/mini.bufremove",
-      keys = {
-        {
-          "<leader>bd",
-          function()
-            local bd = require("mini.bufremove").delete
-            if vim.bo.modified then
-              local choice = vim.fn.confirm(
-                ("Save changes to %q?"):format(vim.fn.bufname()),
-                "&Yes\n&No\n&Cancel"
-              )
-              if choice == 1 then -- Yes
-                vim.cmd.write()
-                bd(0)
-              elseif choice == 2 then -- No
-                bd(0, true)
-              end
-            else
+    "echasnovski/mini.bufremove",
+    keys = {
+      {
+        "<leader>bd",
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(
+              ("Save changes to %q?"):format(vim.fn.bufname()),
+              "&Yes\n&No\n&Cancel"
+            )
+            if choice == 1 then -- Yes
+              vim.cmd.write()
               bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
             end
-          end,
-          desc = "Delete Buffer New",
-        },
-        {
-          "<leader>bD",
-          function() require("mini.bufremove").delete(0, true) end,
-          desc = "Delete Buffer (Force) New",
-        },
+          else
+            bd(0)
+          end
+        end,
+        desc = "Delete Buffer New",
+      },
+      {
+        "<leader>bD",
+        function() require("mini.bufremove").delete(0, true) end,
+        desc = "Delete Buffer (Force) New",
       },
     },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    dependencies = { "echasnovski/mini.bufremove" },
     keys = {
       {
         "<leader>bp",
@@ -107,12 +108,14 @@ return {
           show_buffer_icons = false,
           show_buffer_close_icons = false,
           show_close_icon = false,
+          show_duplicate_prefix = true,
+          max_prefix_length = 100,
           move_wraps_at_ends = false,
           separator_style = { "", "" },
           always_show_bufferline = true,
           hover = { enabled = false },
         },
       }
-    end,
+    end
   }
 }
