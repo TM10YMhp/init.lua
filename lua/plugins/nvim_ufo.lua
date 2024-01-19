1545ded05cc2ee0d51b80bb9d7fc7fd1edf733db
tm10ymhp <tm10ymhp@protonmail.com>
2024-01-19 14:50:46 -0500

    fix nvim-ufo

--- lua/plugins/nvim_ufo.lua
+++ lua/plugins/nvim_ufo.lua
@@ -21,14 +21,14 @@ return {
   {
     "kevinhwang91/nvim-ufo",
     dependencies = { "kevinhwang91/promise-async" },
-    -- event = "VeryLazy",
-    event = function()
-      local bufname = vim.api.nvim_buf_get_name(0)
-      if bufname == "" then
-        return { "BufLeave" }
-      end
-      return { "VeryLazy" }
-    end,
+    event = "VeryLazy",
+    -- event = function()
+    --   local bufname = vim.api.nvim_buf_get_name(0)
+    --   if bufname == "" then
+    --     return { "BufLeave" }
+    --   end
+    --   return { "VeryLazy" }
+    -- end,
     keys = {
       {
         'zR',
