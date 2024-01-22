67dc2d8d0120de5b317565a004292f755609044c
tm10ymhp <tm10ymhp@protonmail.com>
2024-01-22 16:39:01 -0500

    restore startuptime

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,7 +47,7 @@ return {
     "dstein64/vim-startuptime",
     cmd = "StartupTime",
     init = function()
-      vim.g.startuptime_tries = 5
+      vim.g.startuptime_tries = 10
     end
   },
   {
