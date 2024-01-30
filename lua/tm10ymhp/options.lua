e9560b92dee2bc404fae7b9145033e8559c677e1
tm10ymhp <tm10ymhp@protonmail.com>
2024-01-30 11:38:42 -0500

    24.0130.1138

--- lua/tm10ymhp/options.lua
+++ lua/tm10ymhp/options.lua
@@ -10,15 +10,30 @@ vim.g.matchparen_timeout = 20
 vim.g.matchparen_insert_timeout = 20
 vim.g.loaded_matchparen = 1
 
-vim.g.loaded_matchit = 1
+vim.g.markdown_recommended_style = 0
 
+-- clean
+vim.g.editorconfig = false
+vim.g.loaded_man = false
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
 vim.g.loaded_zipPlugin = 1
-vim.g.loaded_zip       = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
 
-vim.g.markdown_recommended_style = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
 
 -- options
 vim.opt.mouse = ""
