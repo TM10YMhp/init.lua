68abec02a885f521ada3ea19f3d96aa5228486a5
tm10ymhp <tm10ymhp@protonmail.com>
2024-02-03 17:46:01 -0500

    24.0203.1746

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,71 +4,73 @@ return {
   opts = function()
     local function template_diagnostic(sources)
       return {
-        'diagnostics',
+        "diagnostics",
         sources = { sources },
-        fmt = function(str) return str:gsub(':', '') end
+        fmt = function(str)
+          return str:gsub(":", "")
+        end,
       }
     end
 
     local function lsp_client_names()
-      local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+      local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
       local clients = vim.lsp.get_active_clients()
 
       if next(clients) == nil then
         return buf_ft
       end
-      return '['..#clients..']'..buf_ft
+      return "[" .. #clients .. "]" .. buf_ft
     end
 
     local function cursor_position()
-      if
-        vim.fn.getfsize(vim.fn.expand('%')) > 1024 * 1024
-      then
-        return "File too long"
+      if vim.fn.getfsize(vim.fn.expand("%")) > 1024 * 1024 then
+        return "Bigfile"
       else
         return '%l:%v|%{virtcol("$")-1}'
       end
     end
 
     local filesize = {
-      'filesize',
-      fmt = function(str) return str.upper(str) end
+      "filesize",
+      fmt = function(str)
+        return str.upper(str)
+      end,
     }
 
     local winbar_config = {
       lualine_c = {
         {
           function()
-            local data = ''
+            local data = ""
 
-            if vim.api.nvim_buf_get_option(0, 'buftype') == '' then
-              data = vim.fn.expand('%:~:.') or '[No Name]'
+            if vim.api.nvim_buf_get_option(0, "buftype") == "" then
+              data = vim.fn.expand("%:~:.") or "[No Name]"
             else
-              data = vim.fn.expand('%:t')
+              data = vim.fn.expand("%:t")
             end
 
-            if data == '' then
-              data = '[No Name]'
+            if data == "" then
+              data = "[No Name]"
             end
 
-            return '> '..data
+            return "> " .. data
           end,
           padding = 0,
           color = "WinBar",
-        }
-      }
+        },
+      },
     }
 
     return {
       options = {
         icons_enabled = false,
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         globalstatus = true,
         refresh = {
           statusline = 1000,
-          tabline    = 0,
-          winbar     = 0,
+          tabline = 0,
+          winbar = 0,
         },
         disabled_filetypes = {
           statusline = {},
@@ -77,42 +79,47 @@ return {
       },
       sections = {
         lualine_a = {
-          { 'mode', fmt = function(str) return str:sub(1,1) end }
+          {
+            "mode",
+            fmt = function(str)
+              return str:sub(1, 1)
+            end,
+          },
         },
-        lualine_b = { 'b:gitsigns_head' },
+        lualine_b = { "b:gitsigns_head" },
         lualine_c = { cursor_position },
         lualine_x = {
           {
-            'diff',
+            "diff",
             source = function()
               local gitsigns = vim.b.gitsigns_status_dict
               if gitsigns then
                 return {
                   added = gitsigns.added,
                   modified = gitsigns.changed,
-                  removed = gitsigns.removed
+                  removed = gitsigns.removed,
                 }
               end
-            end
+            end,
           },
-          template_diagnostic('nvim_workspace_diagnostic'),
-          'o:encoding',
-          'o:fileformat',
-          lsp_client_names
+          template_diagnostic("nvim_workspace_diagnostic"),
+          "o:encoding",
+          "o:fileformat",
+          lsp_client_names,
         },
         lualine_y = { filesize },
-        lualine_z = {'%L'},
+        lualine_z = { "%L" },
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
         lualine_c = { cursor_position },
         lualine_x = { filesize },
-        lualine_y = {'%L'},
+        lualine_y = { "%L" },
         lualine_z = {},
       },
       winbar = winbar_config,
       inactive_winbar = winbar_config,
     }
-  end
+  end,
 }
