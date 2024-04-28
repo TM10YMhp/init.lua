b50207f2a6aca86ffc216a0afe7511b68efad7df
tm10ymhp <tm10ymhp@protonmail.com>
2024-04-28 12:53:06 -0500

    24.0428.1253

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,19 @@
 return {
   {
+    "kawre/neotab.nvim",
+    event = "InsertEnter",
+    opts = {
+      tabkey = "",
+      smart_punctuators = {
+        enabled = true,
+        semicolon = {
+          enabled = true,
+          ft = { "cs", "c", "cpp", "java" },
+        },
+      },
+    },
+  },
+  {
     "tpope/vim-eunuch",
     -- stylua: ignore
     cmd = {
