ad8c844bb1d69b7153cfc329be62db86dc5bd4e1
tm10ymhp <tm10ymhp@protonmail.com>
2024-04-29 19:39:03 -0500

    24.0429.1939

--- lua/servers/jdtls.lua
+++ lua/servers/jdtls.lua
@@ -1,5 +1,6 @@
-local jdtls_path = vim.fn.stdpath('data') .. "/mason/packages/jdtls"
-local workspace_path = vim.fn.stdpath('data') .. '/.cache/jdtls/workspace/'
+local jdtls_path =
+  require("mason-registry").get_package("jdtls"):get_install_path()
+local workspace_path = vim.fn.stdpath("data") .. "/.cache/jdtls/workspace/"
 
 local os = "linux"
 if vim.fn.has("win32") == 1 then
@@ -9,9 +10,10 @@ elseif vim.fn.has("mac") == 1 then
 end
 
 local path_to_lsp_server = jdtls_path .. "/config_" .. os
-local path_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+local path_jar =
+  vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
 
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = workspace_path .. project_name
 
 return {
@@ -19,51 +21,51 @@ return {
   setup = function()
     return {
       cmd = {
-        'java',
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-Xmx1G',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-        '-jar', path_jar,
-        '-configuration', path_to_lsp_server,
-        '-data', workspace_dir,
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xmx1G",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-jar",
+        path_jar,
+        "-configuration",
+        path_to_lsp_server,
+        "-data",
+        workspace_dir,
       },
-      root_dir = require('lspconfig').util.root_pattern(
-        ".git", "mvnw", "gradlew", "pom.xml", "build.gradle"
+      root_dir = require("lspconfig").util.root_pattern(
+        ".git",
+        "mvnw",
+        "gradlew",
+        --
+        "build.xml", -- Ant
+        "pom.xml", -- Maven
+        "settings.gradle", -- Gradle
+        "settings.gradle.kts", -- Gradle
+        -- Multi-module projects
+        "build.gradle",
+        "build.gradle.kts"
       ),
       settings = {
         java = {
-          configuration = {
-            updateBuildConfiguration = "disabled",
-            maven = { userSettings = nil }
-          },
-          errors = { incompleteClasspath = { severity = "warning" } },
+          eclipse = { downloadSources = true },
+          configuration = { updateBuildConfiguration = "interactive" },
           trace = { server = "off" },
-          import = {
-            gradle = { enabled = true },
-            maven = { enabled = true },
-            exclusions = {
-              "**/node_modules/**",
-              "**/.metadata/**",
-              "**/archetype-resources/**",
-              "**/META-INF/maven/**",
-              "/**/test/**"
-            }
-          },
-          referencesCodeLens = { enabled = false },
+          maven = { downloadSources = true },
+          implementationsCodeLens = { enabled = true },
+          referencesCodeLens = { enabled = true },
+          references = { includeDecompiledSources = true },
           signatureHelp = { enabled = true },
-          implementationsCodeLens = { enabled = false },
           format = { enabled = true },
-          saveActions = { organizeImports = false },
-          contentProvider = { preferred = nil },
-          autobuild = { enabled = false },
-        }
-      }
+        },
+      },
     }
-  end
+  end,
 }
