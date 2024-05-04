d16b086e661738ca1f8b6e6fb7738a4023cda612
tm10ymhp <tm10ymhp@protonmail.com>
2024-05-04 10:05:37 -0500

    24.0504.1005

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "dmmulroy/tsc.nvim",
+    cmd = "TSC",
+    config = function()
+      require("tsc.utils").find_tsconfigs = function()
+        local tsconfig = vim.fn.findfile("tsconfig.json", ".;")
+        local jsconfig = vim.fn.findfile("jsconfig.json", ".;")
+
+        if tsconfig ~= "" then
+          return { tsconfig }
+        end
+
+        if jsconfig ~= "" then
+          return { jsconfig }
+        end
+
+        return {}
+      end
+
+      require("tsc").setup({
+        bin_path = "tsc.CMD",
+      })
+    end,
+  },
+  {
     "kawre/neotab.nvim",
     event = "InsertEnter",
     opts = {
