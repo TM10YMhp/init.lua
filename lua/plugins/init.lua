2b1a3f52a7cc9e30db34022ed678e761c53f0d3c
tm10ymhp <tm10ymhp@protonmail.com>
2024-05-20 21:02:28 -0500

    24.0520.2102

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,36 @@
 return {
   {
+    "folke/ts-comments.nvim",
+    keys = {
+      {
+        "gc",
+        function()
+          return require("vim._comment").operator()
+        end,
+        expr = true,
+        mode = { "n", "x" },
+        desc = "Toggle comment",
+      },
+      {
+        "gcc",
+        function()
+          return require("vim._comment").operator() .. "_"
+        end,
+        expr = true,
+        desc = "Toggle comment line",
+      },
+      {
+        "gc",
+        function()
+          require("vim._comment").textobject()
+        end,
+        mode = { "o" },
+        desc = "Comment textobject",
+      },
+    },
+    opts = {},
+  },
+  {
     "kawre/neotab.nvim",
     event = "InsertEnter",
     opts = {
