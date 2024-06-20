e777774884045154ef11a0ad58f09539eae7e117
tm10ymhp <tm10ymhp@protonmail.com>
2024-06-20 12:07:23 -0500

    24.0620.1207

--- lua/tm10ymhp/util/lualine.lua
+++ lua/tm10ymhp/util/lualine.lua
@@ -1,14 +1,5 @@
 local lualine_utils = require("lualine.utils.utils")
 
-local get_hl_from_hllist = function(hllist)
-  for _, hl_name in ipairs(hllist) do
-    if vim.fn.hlexists(hl_name) ~= 0 then
-      return hl_name
-    end
-  end
-  return "Ignore"
-end
-
 local M = {}
 
 M.lsp_client_names = function()
@@ -39,26 +30,42 @@ end
 M.workspace_diagnostics = {
   "diagnostics",
   diagnostics_color = {
-    error = get_hl_from_hllist({
-      "DiagnosticError",
-      "LspDiagnosticsDefaultError",
-      "DiffDelete",
-    }),
-    warn = get_hl_from_hllist({
-      "DiagnosticWarn",
-      "LspDiagnosticsDefaultWarning",
-      "DiffText",
-    }),
-    info = get_hl_from_hllist({
-      "DiagnosticInfo",
-      "LspDiagnosticsDefaultInformation",
-      "Normal",
-    }),
-    hint = get_hl_from_hllist({
-      "DiagnosticHint",
-      "LspDiagnosticsDefaultHint",
-      "DiffChange",
-    }),
+    error = function()
+      return {
+        fg = lualine_utils.extract_color_from_hllist(
+          { "fg", "sp" },
+          { "DiagnosticError", "LspDiagnosticsDefaultError", "DiffDelete" },
+          "none"
+        ),
+      }
+    end,
+    warn = function()
+      return {
+        fg = lualine_utils.extract_color_from_hllist(
+          { "fg", "sp" },
+          { "DiagnosticWarn", "LspDiagnosticsDefaultWarning", "DiffText" },
+          "none"
+        ),
+      }
+    end,
+    info = function()
+      return {
+        fg = lualine_utils.extract_color_from_hllist(
+          { "fg", "sp" },
+          { "DiagnosticInfo", "LspDiagnosticsDefaultInformation", "Normal" },
+          "none"
+        ),
+      }
+    end,
+    hint = function()
+      return {
+        fg = lualine_utils.extract_color_from_hllist(
+          { "fg", "sp" },
+          { "DiagnosticHint", "LspDiagnosticsDefaultHint", "DiffChange" },
+          "none"
+        ),
+      }
+    end,
   },
   sources = { "nvim_workspace_diagnostic" },
   fmt = function(str)
@@ -68,46 +75,40 @@ M.workspace_diagnostics = {
 
 M.diff = {
   "diff",
-  -- HACK: reload diff colors
-  reload_color = {
-    added = {
-      fg = function()
-        return lualine_utils.extract_color_from_hllist("fg", {
+  diff_color = {
+    added = function()
+      return {
+        fg = lualine_utils.extract_color_from_hllist("fg", {
           "LuaLineDiffAdd",
           "GitSignsAdd",
           "GitGutterAdd",
           "DiffAdded",
           "DiffAdd",
-        }, "none")
-      end,
-    },
-    modified = {
-      fg = function()
-        return lualine_utils.extract_color_from_hllist("fg", {
+        }, "none"),
+      }
+    end,
+    modified = function()
+      return {
+        fg = lualine_utils.extract_color_from_hllist("fg", {
           "LuaLineDiffChange",
           "GitSignsChange",
           "GitGutterChange",
           "DiffChanged",
           "DiffChange",
-        }, "none")
-      end,
-    },
-    removed = {
-      fg = function()
-        return lualine_utils.extract_color_from_hllist("fg", {
+        }, "none"),
+      }
+    end,
+    removed = function()
+      return {
+        fg = lualine_utils.extract_color_from_hllist("fg", {
           "LuaLineDiffDelete",
           "GitSignsDelete",
           "GitGutterDelete",
           "DiffRemoved",
           "DiffDelete",
-        }, "none")
-      end,
-    },
-  },
-  diff_color = {
-    added = {},
-    modified = {},
-    removed = {},
+        }, "none"),
+      }
+    end,
   },
   source = function()
     local gitsigns = vim.b.gitsigns_status_dict
@@ -120,9 +121,5 @@ M.diff = {
     end
   end,
 }
-
-M.diff.diff_color.added.fg = M.diff.reload_color.added.fg()
-M.diff.diff_color.modified.fg = M.diff.reload_color.modified.fg()
-M.diff.diff_color.removed.fg = M.diff.reload_color.removed.fg()
 
 return M
