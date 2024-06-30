dd458adb4fba288bf440a937a673c7a4ac88b472
tm10ymhp <tm10ymhp@protonmail.com>
2024-06-30 14:20:17 -0500

    24.0630.1420

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -77,7 +77,6 @@ return {
   {
     "Eandrju/cellular-automaton.nvim",
     cmd = "CellularAutomaton",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
     keys = {
       {
         "<leader>zr",
