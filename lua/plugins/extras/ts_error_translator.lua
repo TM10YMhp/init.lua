18b2b1e061db473c70c84e5c355a8624e98eb346
tm10ymhp <tm10ymhp@protonmail.com>
2024-07-16 16:55:24 -0500

    24.0716.1655

--- lua/plugins/extras/ts_error_translator.lua
+++ lua/plugins/extras/ts_error_translator.lua
@@ -1,6 +1,15 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    { "dmmulroy/ts-error-translator.nvim", opts = {} },
+  { "dmmulroy/ts-error-translator.nvim", lazy = true },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {
+          on_new_config = function(new_config)
+            require("ts-error-translator").setup()
+          end,
+        },
+      },
+    },
   },
 }
