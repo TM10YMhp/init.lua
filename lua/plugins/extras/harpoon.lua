51397b7b0d66ced453411b4ca3a4a91ad31e081c
tm10ymhp <tm10ymhp@protonmail.com>
2024-07-20 14:43:55 -0500

    24.0720.1443

--- lua/plugins/extras/harpoon.lua
+++ lua/plugins/extras/harpoon.lua
@@ -1,62 +1,64 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  keys = {
-    {
-      "<leader>ma",
-      function()
-        require("harpoon"):list():add()
-      end,
-      desc = "Harpoon: Add File",
+  { "nvim-lua/plenary.nvim", lazy = true },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    keys = {
+      {
+        "<leader>ma",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Harpoon: Add File",
+      },
+      {
+        "<leader>mm",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon: Quick Menu",
+      },
+      {
+        "<leader>m1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "Harpoon: Nav File 1",
+      },
+      {
+        "<leader>m2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "Harpoon: Nav File 2",
+      },
+      {
+        "<leader>m3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "Harpoon: Nav File 3",
+      },
+      {
+        "<leader>m4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "Harpoon: Nav File 4",
+      },
+      {
+        "<leader>m5",
+        function()
+          require("harpoon"):list():select(5)
+        end,
+        desc = "Harpoon: Nav File 5",
+      },
     },
-    {
-      "<leader>mm",
-      function()
-        local harpoon = require("harpoon")
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = "Harpoon: Quick Menu",
-    },
-    {
-      "<leader>m1",
-      function()
-        require("harpoon"):list():select(1)
-      end,
-      desc = "Harpoon: Nav File 1",
-    },
-    {
-      "<leader>m2",
-      function()
-        require("harpoon"):list():select(2)
-      end,
-      desc = "Harpoon: Nav File 2",
-    },
-    {
-      "<leader>m3",
-      function()
-        require("harpoon"):list():select(3)
-      end,
-      desc = "Harpoon: Nav File 3",
-    },
-    {
-      "<leader>m4",
-      function()
-        require("harpoon"):list():select(4)
-      end,
-      desc = "Harpoon: Nav File 4",
-    },
-    {
-      "<leader>m5",
-      function()
-        require("harpoon"):list():select(5)
-      end,
-      desc = "Harpoon: Nav File 5",
-    },
-  },
-  opts = {
-    settings = {
-      save_on_toggle = true,
+    opts = {
+      settings = {
+        save_on_toggle = true,
+      },
     },
   },
 }
--- lua/plugins/extras/obsidian.lua
+++ lua/plugins/extras/obsidian.lua
@@ -1,72 +1,82 @@
 return {
-  "epwalsh/obsidian.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  keys = {
-    {
-      "<leader>oe",
-      "<cmd>Neotree dir=" .. SereneNvim.config.extras.obsidian_dir .. "<cr>",
-      desc = "Obsidian Explorer",
-    },
-    { "<leader>oT", "<cmd>ObsidianTemplate<cr>", desc = "Obsidian Template" },
-    --
-    { "<leader>od", "<cmd>ObsidianToday<cr>", desc = "Obsidian Today" },
-    { "<leader>oy", "<cmd>ObsidianPasteImg<cr>", desc = "Obsidian Paste Img" },
-    --
-    {
-      "<leader>oo",
-      "<cmd>ObsidianQuickSwitch<cr>",
-      desc = "Obsidian Quick Switch",
-    },
-    { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Obsidian Search" },
-    { "<leader>ol", "<cmd>ObsidianLinks<cr>", desc = "Obsidian Links" },
-    { "<leader>ot", "<cmd>ObsidianTags<cr>", desc = "Obsidian Backlinks" },
-    { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Obsidian Backlinks" },
-    {
-      "<leader>of",
-      "<cmd>ObsidianFollowLink<cr>",
-      desc = "Obsidian Follow Link",
-    },
-  },
-  opts = {
-    workspaces = {
+  { "nvim-lua/plenary.nvim", lazy = true },
+  {
+    "epwalsh/obsidian.nvim",
+    keys = {
       {
-        name = "personal",
-        path = SereneNvim.config.extras.obsidian_dir,
+        "<leader>oe",
+        "<cmd>Neotree dir=" .. SereneNvim.config.extras.obsidian_dir .. "<cr>",
+        desc = "Obsidian Explorer",
+      },
+      { "<leader>oT", "<cmd>ObsidianTemplate<cr>", desc = "Obsidian Template" },
+      --
+      { "<leader>od", "<cmd>ObsidianToday<cr>", desc = "Obsidian Today" },
+      {
+        "<leader>oy",
+        "<cmd>ObsidianPasteImg<cr>",
+        desc = "Obsidian Paste Img",
+      },
+      --
+      {
+        "<leader>oo",
+        "<cmd>ObsidianQuickSwitch<cr>",
+        desc = "Obsidian Quick Switch",
+      },
+      { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Obsidian Search" },
+      { "<leader>ol", "<cmd>ObsidianLinks<cr>", desc = "Obsidian Links" },
+      { "<leader>ot", "<cmd>ObsidianTags<cr>", desc = "Obsidian Backlinks" },
+      {
+        "<leader>ob",
+        "<cmd>ObsidianBacklinks<cr>",
+        desc = "Obsidian Backlinks",
+      },
+      {
+        "<leader>of",
+        "<cmd>ObsidianFollowLink<cr>",
+        desc = "Obsidian Follow Link",
       },
     },
-    notes_subdir = "inbox",
-    daily_notes = {
-      folder = "journal",
-      date_format = "%yw%W",
-      template = "weekly_report_tmpl.md",
-    },
-    mappings = {},
-    note_id_func = function(title)
-      local suffix = ""
-      if title ~= nil then
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      else
-        suffix = tostring(os.time())
-      end
-      return suffix
-    end,
-    preferred_link_style = "markdown",
-    disable_frontmatter = true,
-    templates = {
-      folder = "templates",
-      substitutions = {
-        ["date:YY\\wWW"] = function()
-          return os.date("%yw%W")
-        end,
-        ["date:YY.MMDD"] = function()
-          return os.date("%y.%m%d")
-        end,
-        ["time:HHmm|ss"] = function()
-          return os.date("%H%M|%S")
-        end,
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = SereneNvim.config.extras.obsidian_dir,
+        },
       },
+      notes_subdir = "inbox",
+      daily_notes = {
+        folder = "journal",
+        date_format = "%yw%W",
+        template = "weekly_report_tmpl.md",
+      },
+      mappings = {},
+      note_id_func = function(title)
+        local suffix = ""
+        if title ~= nil then
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          suffix = tostring(os.time())
+        end
+        return suffix
+      end,
+      preferred_link_style = "markdown",
+      disable_frontmatter = true,
+      templates = {
+        folder = "templates",
+        substitutions = {
+          ["date:YY\\wWW"] = function()
+            return os.date("%yw%W")
+          end,
+          ["date:YY.MMDD"] = function()
+            return os.date("%y.%m%d")
+          end,
+          ["time:HHmm|ss"] = function()
+            return os.date("%H%M|%S")
+          end,
+        },
+      },
+      ui = { enable = false },
+      attachments = { img_folder = "files" },
     },
-    ui = { enable = false },
-    attachments = { img_folder = "files" },
   },
 }
