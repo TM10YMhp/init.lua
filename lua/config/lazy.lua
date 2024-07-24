221399bf5e64b11a086e872eca1bd3c3dfece964
tm10ymhp <tm10ymhp@protonmail.com>
2024-07-23 20:30:21 -0500

    24.0723.2030

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -118,6 +118,25 @@ vim.api.nvim_create_autocmd("User", {
       vim.opt.clipboard:prepend({ "unnamed", "unnamedplus" })
     end
 
+    -- https://github.com/neovim/neovim/discussions/28010#discussioncomment-9877494
+    local function paste()
+      return {
+        vim.fn.split(vim.fn.getreg(""), "\n"),
+        vim.fn.getregtype(""),
+      }
+    end
+    vim.g.clipboard = {
+      name = "OSC 52",
+      copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+      },
+      paste = {
+        ["+"] = paste,
+        ["*"] = paste,
+      },
+    }
+
     require("config.diagnostic")
     require("config.keymaps")
 
