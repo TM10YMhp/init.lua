b00798b975a993b5f9145bd47bc2ad6c3d1d68a2
tm10ymhp <tm10ymhp@protonmail.com>
2024-07-28 21:30:51 -0500

    24.0728.2130

--- lua/plugins/extras/dadbod.lua
+++ lua/plugins/extras/dadbod.lua
@@ -1,33 +1,45 @@
 return {
-  "kristijanhusak/vim-dadbod-ui",
-  enabled = false,
-  dependencies = { "tpope/vim-dadbod", cmd = "DB" },
-  cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
-  keys = {
-    { "<leader>db", "<cmd>DBUI<cr>", desc = "DBUI" },
+  {
+    "tpope/vim-dadbod",
+    cmd = "DB",
   },
-  init = function()
-    vim.g.db_ui_win_position = "right"
-    vim.g.db_ui_icons = {
-      expanded = {
-        db = "-",
-        buffers = "-",
-        saved_queries = "-",
-        schemas = "-",
-        schema = "-",
-        tables = "-",
-        table = "-",
-      },
-      collapsed = {
-        db = "+",
-        buffers = "+",
-        saved_queries = "+",
-        schemas = "+",
-        schema = "+",
-        tables = "+",
-        table = "+",
-      },
-    }
-    vim.g.db_ui_force_echo_notifications = 1
-  end,
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = { "tpope/vim-dadbod" },
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    keys = {
+      { "<leader>db", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+    },
+    init = function()
+      local data_path = vim.fn.stdpath("data")
+
+      vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+      vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+      -- vim.g.db_ui_use_nvim_notify = true
+      vim.g.db_ui_execute_on_save = false
+
+      vim.g.db_ui_win_position = "right"
+      vim.g.db_ui_icons = {
+        expanded = {
+          db = "-",
+          buffers = "-",
+          saved_queries = "-",
+          schemas = "-",
+          schema = "-",
+          tables = "-",
+          table = "-",
+        },
+        collapsed = {
+          db = "+",
+          buffers = "+",
+          saved_queries = "+",
+          schemas = "+",
+          schema = "+",
+          tables = "+",
+          table = "+",
+        },
+      }
+      vim.g.db_ui_force_echo_notifications = 1
+    end,
+  },
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,9 @@ return {
   {
     "mbbill/undotree",
     cmd = "UndotreeToggle",
+    keys = {
+      { "<leader>fg", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
+    },
     config = function()
       vim.g.undotree_WindowLayout = 4
       vim.g.undotree_DiffpanelHeight = 30
