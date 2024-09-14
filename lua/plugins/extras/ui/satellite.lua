9d3a08cc494446a4df4a17884aa0bc8f890fccfe
tm10ymhp <tm10ymhp@protonmail.com>
2024-09-14 09:55:28 -0500

    24.0914.0955

--- lua/plugins/extras/ui/satellite.lua
+++ lua/plugins/extras/ui/satellite.lua
@@ -2,8 +2,10 @@ return {
   "lewis6991/satellite.nvim",
   event = "VeryLazy",
   opts = {
+    winblend = 0,
     handlers = {
       gitsigns = {
+        overlap = true,
         signs = SereneNvim.config.icons.gitsigns,
       },
       marks = {
