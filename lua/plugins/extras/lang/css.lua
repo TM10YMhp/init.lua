1191b6cd6d7a4299ee5e0fb4030927f0f5841c70
tm10ymhp <tm10ymhp@protonmail.com>
2024-09-16 12:19:14 -0500

    24.0916.1219

--- lua/plugins/extras/lang/css.lua
+++ lua/plugins/extras/lang/css.lua
@@ -1,4 +1,4 @@
--- TODO: wip
+-- TODO: WIP
 return {
   {
     "nvim-treesitter/nvim-treesitter",
--- lua/plugins/extras/ui/satellite.lua
+++ lua/plugins/extras/ui/satellite.lua
@@ -1,27 +1,61 @@
--- TODO: replace
+-- TODO: WIP
 return {
-  "lewis6991/satellite.nvim",
-  event = "VeryLazy",
-  opts = {
-    winblend = 0,
-    handlers = {
-      cursor = {
-        priority = 10,
-        symbols = { " " },
+  {
+    "dstein64/nvim-scrollview",
+    event = "VeryLazy",
+    opts = {
+      winblend = 0,
+      signs_on_startup = {
+        "conflicts",
+        "cursor",
+        "diagnostics",
+        "marks",
+        "search",
       },
-      diagnostic = {
-        enable = false,
-        priority = 100,
-        signs = { "-", "=", "≡" },
-      },
-      gitsigns = {
-        overlap = true,
-        signs = SereneNvim.config.icons.gitsigns,
-      },
-      marks = {
-        enable = false,
-        show_builtins = true,
-      },
+      cursor_symbol = "",
+      cursor_priority = 100,
+      search_symbol = "=",
+      signs_column = 0,
+      signs_max_per_row = 1,
     },
+    config = function(_, opts)
+      vim.api.nvim_set_hl(0, "ScrollView", { link = "CursorLine" })
+      vim.api.nvim_set_hl(0, "ScrollViewCursor", { link = "Visual" })
+
+      require("scrollview.contrib.gitsigns").setup()
+      require("scrollview").setup(opts)
+    end,
   },
+  -- {
+  --   "lewis6991/satellite.nvim",
+  --   -- event = "VeryLazy",
+  --   opts = {
+  --     winblend = 0,
+  --     handlers = {
+  --       cursor = {
+  --         priority = 10,
+  --         symbols = { " " },
+  --       },
+  --       diagnostic = {
+  --         enable = false,
+  --         signs = { "*" },
+  --       },
+  --       gitsigns = {
+  --         enable = false,
+  --         overlap = true,
+  --         signs = SereneNvim.config.icons.gitsigns,
+  --       },
+  --       marks = {
+  --         enable = false,
+  --         show_builtins = true,
+  --       },
+  --       quickfix = {
+  --         enable = false,
+  --       },
+  --       search = {
+  --         enable = false,
+  --       },
+  --     },
+  --   },
+  -- },
 }
