e427552d7bcea4670542f43f525d774dc9fdc7ae
tm10ymhp <tm10ymhp.editor919@slmails.com>
2025-11-16 18:13:21 -0500

    25.1116.1813

--- lua/plugins/presets/_init.lua
+++ lua/plugins/presets/_init.lua
@@ -1,4 +1,4 @@
-if vim.env.USERNAME ~= "qwe" and vim.env.MY_PATH ~= "" then return end
+if vim.env.USERNAME ~= "qwe" and vim.env.MY_PATH ~= "" then return {} end
 
 return {
   { import = "plugins.presets.vscode" },
