fe75870fbfb4f63660e55e6115565e02573c80e8
tm10ymhp <tm10ymhp.editor919@slmails.com>
2025-12-09 12:16:29 -0500

    25.1209.1216

--- lua/plugins/presets/vscode/_init.lua
+++ lua/plugins/presets/vscode/_init.lua
@@ -1,4 +1,6 @@
-if not vim.g.vscode then return {} end
+if not vim.g.vscode then
+  return {}
+end
 
 local enabled = {
   "dial.nvim",
@@ -8,7 +10,6 @@ local enabled = {
   "mini.operators",
   "nvim-surround",
   "nvim-treesitter",
-  "nvim-treesitter-endwise",
   "nvim-treesitter-textobjects",
   "vim-cool",
   "vim-matchup",
@@ -26,6 +27,25 @@ Config.options.change_detection.enabled = false
 Config.options.defaults.cond = function(plugin)
   return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
 end
+
+local vscode = require("vscode")
+
+---@return boolean
+local is_first_startup = function()
+  local name_var = "vscode.vscode_neovim_activated"
+  local activated = vscode.eval(("return !!%s"):format(name_var))
+  if activated then
+    return false
+  end
+  vscode.eval(("%s = true"):format(name_var))
+  return true
+end
+
+if is_first_startup() then
+  vscode.action("vscode-neovim.stop")
+end
+
+vim.notify = vscode.notify
 
 return {
   {
