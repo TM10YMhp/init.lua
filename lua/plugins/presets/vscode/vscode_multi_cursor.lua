cb1eb81f04387315ec3e25f9aade87f7d08fd1cb
tm10ymhp <tm10ymhp.editor919@slmails.com>
2025-12-30 21:20:15 -0500

    25.1230.2120

--- lua/plugins/presets/vscode/vscode_multi_cursor.lua
+++ lua/plugins/presets/vscode/vscode_multi_cursor.lua
@@ -2,5 +2,8 @@ return {
   "vscode-neovim/vscode-multi-cursor.nvim",
   event = "VeryLazy",
   cond = not not vim.g.vscode,
+  keys = {
+    { "<leader>mn", "mciw*<Cmd>nohl<CR>", remap = true },
+  },
   opts = {},
 }
