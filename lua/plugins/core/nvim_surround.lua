29d65358d87e6c973f2e28aed73a87b1b554e4e0
tm10ymhp <tm10ymhp.editor919@slmails.com>
2026-03-27 21:26:01 -0500

    26.0327.2126

--- lua/plugins/core/nvim_surround.lua
+++ lua/plugins/core/nvim_surround.lua
@@ -1,20 +1,44 @@
 return {
   "kylechui/nvim-surround",
-  event = "VeryLazy",
+  -- event = "VeryLazy",
+  init = function()
+    vim.g.nvim_surround_no_mappings = true
+  end,
+  keys = {
+    {
+      "ys",
+      "<Plug>(nvim-surround-normal)",
+      desc = "Add a surrounding pair around a motion (normal mode)"
+    },
+    {
+      "ds",
+      "<Plug>(nvim-surround-delete)",
+      desc = "Delete a surrounding pair"
+    },
+    {
+      "cs",
+      "<Plug>(nvim-surround-change)",
+      desc = "Change a surrounding pair"
+    },
+    {
+      "cS",
+      "<Plug>(nvim-surround-change-line)",
+      desc = "Change a surrounding pair, putting replacements on new lines"
+    },
+    {
+      "S",
+      "<Plug>(nvim-surround-visual)",
+      desc = "Add a surrounding pair around a visual selection",
+      mode = { "x" }
+    },
+    {
+      "gS",
+      "<Plug>(nvim-surround-visual-line)",
+      desc = "Add a surrounding pair around a visual selection, on new lines",
+      mode = { "x" },
+    },
+  },
   opts = {
     move_cursor = "sticky",
-    keymaps = {
-      insert = false,
-      insert_line = false,
-      normal = "ys",
-      normal_cur = false,
-      normal_line = false,
-      normal_cur_line = false,
-      visual = "S",
-      visual_line = false,
-      delete = "ds",
-      change = "cs",
-      change_line = false,
-    },
   },
 }
--- lua/plugins/extras/util/cloak.lua
+++ lua/plugins/extras/util/cloak.lua
@@ -1,63 +1,35 @@
-SereneNvim.on_lazy_init(function()
-  vim.filetype.add({
-    pattern = {
-      -- match filenames like - ".env.example", ".env.local" and so on
-      ["%.env%.[%w_.-]+"] = "sh",
-    },
-  })
-end)
+-- SereneNvim.on_lazy_init(function()
+--   vim.filetype.add({
+--     pattern = {
+--       -- match filenames like - ".env.example", ".env.local" and so on
+--       ["%.env%.[%w_.-]+"] = "sh",
+--     },
+--   })
+-- end)
 
 return {
-  {
-    "laytan/cloak.nvim",
-    ft = { "sh", "minifiles" },
-    init = function()
-      SereneNvim.on_very_lazy(function()
-        Snacks.toggle
-          .new({
-            name = "Shelter",
-            get = function() return require("cloak").opts.enabled end,
-            set = function(state)
-              local cloak = require("cloak")
-              if state then
-                cloak.enable()
-              else
-                cloak.disable()
-              end
-            end,
-          })
-          :map("<leader>oC")
-      end)
-    end,
-    opts = {
-      enabled = true,
-      cloak_character = "*",
-      cloak_telescope = false,
-    },
-  },
-  {
-    "ph1losof/ecolog.nvim",
-    event = "VeryLazy",
-    opts = {
-      shelter = {
-        modules = {
-          -- cmp = true,
-          -- peek = true,
-          files = false, -- FIX: not work
-          -- telescope = true,
-          -- telescope_previewer = true,
-          -- fzf = true,
-          -- fzf_previewer = true,
-          snacks = true,
-          snacks_previewer = true,
-        },
-      },
-      integrations = { nvim_cmp = false },
-      types = false,
-      provider_patterns = {
-        extract = false,
-        cmp = false,
-      },
-    },
+  "zeybek/camouflage.nvim",
+  event = "VeryLazy",
+  init = function()
+    SereneNvim.on_very_lazy(function()
+      Snacks.toggle
+        .new({
+          name = "Camouflage",
+          get = function() return require("camouflage").is_enabled() end,
+          set = function(state)
+            local camouflage = require("camouflage")
+            if state then
+              camouflage.enable()
+            else
+              camouflage.disable()
+            end
+          end,
+        })
+        :map("<leader>oC")
+    end)
+  end,
+  opts = {
+    pwned = { enabled = false },
+    project_config = { enabled = false },
   },
 }
